fc634afdd6bba31207ac406d8915b1bd1351a1cc
DenilsonCunha <denilsonluciano6@gmail.com>
2021-05-24 12:32:09 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import Caixas from './Componentes/caixas'
+import estilo from './estilos/estilo.js';
+
+
+export default function App() {
+  let vexibir=true;
+  return (
+    <View style={estilo.conteiner}>
+      <Image
+          source={require('./assets/download.png')}
+          style={estilos.logo}
+      />
+      <Caixas exibir={vexibir}/>
+      {vexibir?<Text>Você está aprendendo react native</Text>:<Text>----</Text>}
+    </View>
+  );
+};
+
+const estilos = StyleSheet.create({
+  logo: {
+    width: 250,
+    resizeMode:'contain'
+  }
+})
