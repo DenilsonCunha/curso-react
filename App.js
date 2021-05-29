f8a92a90c6630c8f1e265e057f3946f2a84f1966
DenilsonCunha <denilsonluciano6@gmail.com>
2021-05-29 15:04:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 
 import React from 'react';
-import { StyleSheet, Text, View, Image,} from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground,} from 'react-native';
 import estilo from './estilos/estilo.js';
 import LP from './Componentes/LP'
 
 export default function App() {
   return (
     <View style={estilo.conteiner}>
+      <ImageBackground 
+       source={require('./assets/download.png')}
+      >
       <Text>Nova fase do Curso</Text>
+      </ImageBackground>
     </View>
   );
 };
