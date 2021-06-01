a909c12a62b8c8809a14deec8e25922905e653d7
DenilsonCunha <denilsonluciano6@gmail.com>
2021-06-01 10:36:10 -0300

    Update App.js

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default function App() {
     <View style={estilo.conteiner}>
       <Button
       title={ligado?'Desligar':'Ligar'}
+      onPress={()=>setLigado(!ligado)}
       />
       {ligado?
       <View>
